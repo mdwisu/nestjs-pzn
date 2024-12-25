9e2d778e8e6710a2cda5e1c21a7e2ffe50885555
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-25 16:04:04 +0700

    feat: shared module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
-import {
-  Connection,
-  createConnection,
-  MongoDBConnection,
-  MySqlConnection,
-} from './connection/connection';
+import { Connection, createConnection } from './connection/connection';
 import { mailService, MailService } from './mail/mail.service';
 import {
   createUserRepository,
@@ -44,5 +39,6 @@ import { ConfigService } from '@nestjs/config';
     },
     MemberService,
   ],
+  exports: [UserService],
 })
 export class UserModule {}
